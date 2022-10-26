67bb2a736040877648516a8cfc7ec1ac82f04d18
Selvyn <swright@celestial.co.uk>
2022-10-26 20:27:13 +0100

    Initial push, code in main()

---
+++ src/main/java/com/celestial/mocking_explored/App.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.celestial.mocking_explored;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author selvy
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        FileReader fr = null;
+        try
+        {
+            String fileName = "C:\\tmp\\KeyboardHandler.java.txt";
+            File file = new File(fileName);
+            fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                //process the line
+                System.out.println(line);
+            }       
+        } catch (IOException ex)
+        {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        finally
+        {
+            try
+            {
+                fr.close();
+            } catch (IOException ex)
+            {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
