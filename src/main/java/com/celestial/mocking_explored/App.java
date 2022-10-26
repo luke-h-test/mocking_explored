b49b096548ef803e7b45e7acc575735f5dfbe433
Selvyn <swright@celestial.co.uk>
2022-10-26 20:48:41 +0100

    Moved file loading into its own class called TextFileLoader

--- src/main/java/com/celestial/mocking_explored/App.java
+++ src/main/java/com/celestial/mocking_explored/App.java
@@ -5,14 +5,6 @@
  */
 package com.celestial.mocking_explored;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author selvy
@@ -21,43 +13,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        Iterable<String> lines = loadFile();
+        TextFileLoader tfl = new TextFileLoader();
+        Iterable<String> lines = tfl.loadFile("C:\\tmp\\KeyboardHandler.java.txt");
         
         lines.forEach((element) ->{
             System.out.println(element);
         });
     }
     
-    public  static  Iterable<String>  loadFile()
-    {
-        FileReader fr = null;
-        ArrayList<String> lines = new ArrayList<>();
-        try
-        {
-            String fileName = "C:\\tmp\\KeyboardHandler.java.txt";
-            File file = new File(fileName);
-            fr = new FileReader(file);
-            BufferedReader br = new BufferedReader(fr);
-            String line;
-            while((line = br.readLine()) != null)
-            {
-                //process the line
-                lines.add(line);
-            }       
-        } catch (IOException ex)
-        {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-        } 
-        finally
-        {
-            try
-            {
-                fr.close();
-            } catch (IOException ex)
-            {
-                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        return lines;
-    }
 }
---
+++ src/main/java/com/celestial/mocking_explored/TextFileLoader.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.celestial.mocking_explored;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author selvy
+ */
+public class TextFileLoader
+{
+    public  Iterable<String>  loadFile(  String fname )
+    {
+        FileReader fr = null;
+        ArrayList<String> lines = new ArrayList<>();
+        try
+        {
+            String fileName = fname;
+            File file = new File(fileName);
+            fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                //process the line
+                lines.add(line);
+            }       
+        } catch (IOException ex)
+        {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        finally
+        {
+            try
+            {
+                fr.close();
+            } catch (IOException ex)
+            {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return lines;
+    }
+    
+}
